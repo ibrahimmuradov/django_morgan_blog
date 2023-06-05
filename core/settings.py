287909103613bbc3fb528f18006b9bdf29712a4e
ibrahim <ibrahimmuradov24@gmail.com>
2023-06-05 19:45:33 +0400

    update seting.py smtp info

--- core/settings.py
+++ core/settings.py
@@ -156,8 +156,8 @@ LOGIN_URL = '/account/login/'
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 
 EMAIL_HOST = "smtp.gmail.com"
-# EMAIL_HOST_USER = '' #  email address
-# EMAIL_HOST_PASSWORD = '' # email pass
+EMAIL_HOST_USER = '' #  email address
+EMAIL_HOST_PASSWORD = '' # email pass
 EMAIL_PORT = 465
 EMAIL_USE_TLS = False
 EMAIL_USE_SSL = True
